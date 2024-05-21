d1b53d149a817271d3f8e2fbae306d56f7b46637
wenliang <liuwenliang001@163.com>
2024-05-21 20:58:01 +0800

    feat: pom优化

--- novel-cast-server/src/main/java/com/example/novelcastserver/NovelCastServerApplication.java
+++ novel-cast-server/src/main/java/com/example/novelcastserver/NovelCastServerApplication.java
@@ -1,7 +1,5 @@
 package com.example.novelcastserver;
 
-import org.bytedeco.javacpp.Loader;
-import org.bytedeco.opencv.opencv_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +8,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class NovelCastServerApplication {
 
     public static void main(String[] args) {
-        Loader.load(opencv_java.class);
         ConfigurableApplicationContext context = SpringApplication.run(NovelCastServerApplication.class, args);
         System.out.println();
         System.out.println(STR."启动成功: http://127.0.0.1:\{context.getEnvironment().getProperty("server.port")}");
