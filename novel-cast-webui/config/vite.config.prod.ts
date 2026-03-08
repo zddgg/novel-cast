3706a98343fe5201c410399dfe08e4dd98d31afc
liuwenliang <liuwenliang001@163.com>
2026-03-09 01:29:37 +0800

    update

--- novel-cast-webui/config/vite.config.prod.ts
+++ novel-cast-webui/config/vite.config.prod.ts
@@ -4,17 +4,6 @@ import baseConfig from './vite.config.base';
 export default mergeConfig(
   {
     mode: 'production',
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            arco: ['@arco-design/web-vue'],
-            vue: ['vue', 'vue-router', 'pinia', '@vueuse/core', 'vue-i18n'],
-          },
-        },
-      },
-      chunkSizeWarningLimit: 2000,
-    },
   },
   baseConfig
 );
--- novel-cast-webui/src/main.ts
+++ novel-cast-webui/src/main.ts
@@ -7,9 +7,7 @@ import router from './router';
 import store from './store';
 import i18n from './locale';
 import App from './App.vue';
-// Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
-// 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
-// https://arco.design/docs/designlab/use-theme-package
+
 import '@/assets/style/global.less';
 import '@/api/interceptor';
 import '@arco-design/web-vue/dist/arco.css';
